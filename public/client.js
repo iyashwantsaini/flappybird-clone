91cdd6104bdbb8ecbbdbff1f63dca27ead59112b
Glitch (hello-express) <none>
2019-09-07 19:57:17 +0000

    📫⏳ Checkpoint ./.glitch-assets:6055465/1120 ./public/client.js:6055465/1003

--- public/client.js
+++ public/client.js
@@ -1,3 +1,14 @@
 var cvs = document.getElementById("canvas");
 var ctx = cvs.getContext("2d");
 
+var bird = new Image();
+var bg = new Image();
+var fg = new Image();
+var pipeNorth = new Image();
+var pipeSouth = new Image();
+
+bird.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fbluebird-midflap.png?v=1567885073227";
+bg.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fback.jpg?v=1567885810251";
+fg.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fbase.png?v=1567886163119";
+pipeNorth.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fpipe-green-down.png?v=1567885972335";
+pipeSouth.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fpipe-green.png?v=1567885058321";
