0726f5012778e3ecf194b0fb802ee988b00969a6
Glitch (hello-express) <none>
2019-09-07 20:32:17 +0000

    ☔️👑 Checkpoint ./.glitch-assets:6055465/752 ./public/client.js:6055465/422

--- public/client.js
+++ public/client.js
@@ -11,8 +11,8 @@ var pipeNorth = new Image();
 var pipeSouth = new Image();
 
 bird.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fbluebird-midflap.png?v=1567885073227";
-bg.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fback.jpg?v=1567885810251";
-fg.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fbase.png?v=1567886163119";
+bg.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fbackfull.png?v=1567888229822";
+fg.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fground.png?v=1567887807620";
 pipeNorth.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fpipe-green-down.png?v=1567887345074";
 pipeSouth.src = "https://cdn.glitch.com/180b6160-0431-44ea-b9dd-f7b9082bb344%2Fpipe-green.png?v=1567885058321";
 
@@ -24,7 +24,8 @@ function draw(){
   ctx.drawImage(bg,0,0);
   ctx.drawImage(pipeNorth,window.innerWidth-200,0);
   ctx.drawImage(pipeSouth,window.innerWidth-200,constant);
-  ctx.drawImage(fg,0,0);
+  ctx.drawImage(fg,0,window.innerHeight-fg.height);
+  ctx.drawImage(bird,100,100);
 }
 
 draw();
